f3ac7af64644d93c81e0de2b4b44ef9a2738871a
mosu001 <michael.osullivan@auckland.ac.nz>
2023-05-05 16:56:50 +1200

    Cleaning code and cleaned CSV added

--- cleaning.R
+++ cleaning.R
@@ -1,2 +1,29 @@
 setwd("C:/Users/mosu001/OneDrive - The University of Auckland/Documents/uoa/industry-engagement/PracWork")
 raw_df <- read.csv("~/uoa/industry-engagement/PracWork/practical_work_registration report 29-03-2023.csv")
+sort_df <- raw_df[order(raw_df$Company.Name),]
+
+invalid = c(
+  "",
+  "...",
+  "5b Kindergarten Dr, Conifer Grove"
+)
+
+valid_df = sort_df[!(sort_df$Company.Name %in% invalid),]
+
+needsChange = c(
+  " (aerospace New Business Networks Company Limited)",
+  "(fujian Liansheng Paper Industry Co., Ltd.)",
+  "9spokes International Limited"
+)
+change <- c(
+  " (aerospace New Business Networks Company Limited)"="Aerospace New Business Networks Company Limited",
+  "(fujian Liansheng Paper Industry Co., Ltd.)"="Fujian Liansheng Paper Industry Co., Ltd.",
+  "9spokes International Limited"="9 Spokes International Limited"
+)
+
+change_df = valid_df
+change_df$Company.Name = ifelse(
+  change_df$Company.Name %in% needsChange,
+  change[change_df$Company.Name],
+  change_df$Company.Name 
+)

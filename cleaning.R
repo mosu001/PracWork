ad5dbe19ef1061e4209bf95c6c22f75fe20ab3ec
mosu001 <michael.osullivan@auckland.ac.nz>
2023-05-05 16:40:35 +1200

    Create cleaning.R

---
+++ cleaning.R
@@ -0,0 +1,2 @@
+setwd("C:/Users/mosu001/OneDrive - The University of Auckland/Documents/uoa/industry-engagement/PracWork")
+raw_df <- read.csv("~/uoa/industry-engagement/PracWork/practical_work_registration report 29-03-2023.csv")
